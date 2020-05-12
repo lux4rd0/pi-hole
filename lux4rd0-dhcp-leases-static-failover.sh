7ab338596758525733e021cd56442f6b4d05ddd2
Dave Schmid <dave@pulpfree.org>
2020-05-12 00:07:09 -0500

    Update lux4rd0-dhcp-leases-static-failover.sh

--- lux4rd0-dhcp-leases-static-failover.sh
+++ lux4rd0-dhcp-leases-static-failover.sh
@@ -7,15 +7,16 @@ do
   client_ip=$(echo $line|awk '{print $1}')
   client_name=$(echo $line|awk '{print $2}')
 
-output="echo $client_ip $client_name.pulpfree.net       $client_name >> /tmp/static-leases.tmp"
+output="echo $client_ip	$client_name.tylephony.com	$client_name >> /tmp/static-leases.tmp"
 eval "$output"
+
 done
 
 sort /tmp/static-leases.tmp > /tmp/lux4rd0-static-leases.lan
 cat /etc/hosts /tmp/lux4rd0-static-leases.lan > /tmp/lux4rd0-hosts
 
-scp /tmp/lux4rd0-static-leases.lan root@pfdns02.pulpfree.net:/etc/pihole
-scp /tmp/lux4rd0-hosts root@pfdns02.pulpfree.net:/etc/hosts
-ssh root@pfdns02.pulpfree.net '/usr/local/bin/pihole restartdns'
+scp /tmp/lux4rd0-static-leases.lan root@dns02.tylephony.com:/etc/pihole
+scp /tmp/lux4rd0-hosts root@dns02.tylephony.com:/etc/hosts
+ssh root@dns02.tylephony.com '/usr/local/bin/pihole restartdns'
 
 rm /tmp/static-leases.tmp
